a63e333c7a4a03f632093b87761c2763ac581b59
CSJ <tmdwns9630@gmail.com>
2022-09-16 11:17:00 +0900

    컴포넌트의 props 사용법 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from "react";
-import Food from "./Food";
 
-const favArr = ["김치", "볶음", "파전"];
+function Movie(props) {
+  console.log(props);
+
+  return (
+    <>
+      <hr></hr>
+      <div>영화명 : {props.movieName}</div>
+      <div>주연배우 : {props.actor}</div>
+      <div>장르 : {props.genre}</div>
+
+      <hr></hr>
+    </>
+  );
+}
 
 function App() {
   return (
     <React.Fragment>
-      <h1 id="one">좋아하는 음식 목록</h1>
-      <ul>
-        {favArr.map((ele) => {
-          return (
-            <li>
-              <Food fav={ele} />
-            </li>
-          );
-        })}
-      </ul>
+      <h1>김나타 영화앱</h1>
+      <Movie movieName="탑건-매버릭" actor="톰크루즈" genre="비행시뮬레이션" />
+      <Movie movieName="스파이더맨 노웨이홈" actor="톰 홀랜드" genre="히어로" />
+      <Movie movieName="좀비랜드 더블탭" actor="우디 해럴슨" genre="좀비" />
     </React.Fragment>
   );
 }
