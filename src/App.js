b12ff41841c6a3549e730370ea45df8bd19f3e10
CSJ <tmdwns9630@gmail.com>
2022-09-20 00:18:23 +0900

    IMF로 확보한 파일을 훑어보고 적당히 고쳐냄. 체크는 안함 ㅎㅎ

--- src/App.js
+++ src/App.js
@@ -20,21 +20,36 @@ function App(props) {
     setMovies(result.data.results);
     // console.log(`무비스 after : ${movies}`);
     // console.log(`무비스0 타이틀 : ${movies[0].title}`);
+    setLoading(false);
   }
 
   //뭔가를 기다려야할 경우
   //함수 : async, 함수가 기다려 줄 수 있는 함수라고 명시해야 한다.
   //await : 기다려야할 대상이 있는 라인을 명시.
   //result.data에 postman에서 봤던 데이터가 객체화되어서 들어와있다.
-  getMovieAPI();
+  useEffect(function () {
+    getMovieAPI();
+  }, []);
 
   return (
     <>
       {loading ? (
-        <div>로딩중..</div>
+        <div>로딩중...</div>
       ) : (
         movies.map(function (ele) {
-          return <Movie key={ele.id} />;
+          return (
+            <Movie
+              key={ele.id}
+              id={ele.id}
+              title={ele.title}
+              poster_path={ele.poster_path}
+              overview={ele.overview}
+              vote_average={ele.vote_average}
+              adult={ele.adult}
+              original_language={ele.original_language}
+              release_date={ele.release_date}
+            />
+          );
         })
       )}
     </>
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,20 +2,30 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 function Movie(props) {
   return (
-    <div className="movie_data">
-      <img
-        className="movie_poster"
-        width={100}
-        src={`https://image.tmdb.org/t/p/original/${props.poster_path}`}
-        alt="loading"
-      ></img>
-      <h3 className="movie_title">{props.title}</h3>
-      <div className="movie_overview">{props.overview.slice(0, 100)}</div>
-      <div className="movie_vote">{props.vote_average}</div>
-      <div className="movie_adult">{props.adult.toString()}</div>
-      <div className="movie_lang">{props.original_language}</div>
-      <div className="movie_date">{props.date}</div>
-      <div className="movie_id">영화ID :{props.id}</div>
+    <div className="movie__data">
+      <div className="movie__poster">
+        <img
+          className="movie__poster_img"
+          width={100}
+          src={`https://image.tmdb.org/t/p/original${props.poster_path}`}
+        ></img>
+        <div className="movie__adult">{!props.adult ? "19" : null}</div>
+      </div>
+
+      <h3 className="movie__title">{props.title}</h3>
+      <div className="movie__overview">{props.overview.slice(0, 100)}...</div>
+      <div className="movie__vote">
+        ({props.vote_average} / 10){" "}
+        {Array(Number(props.vote_average.toFixed(0)))
+          .fill(0)
+          .map((ele, idx) => (
+            <span key={idx}>⭐</span>
+          ))}
+      </div>
+
+      <div className="movie__lang">{props.original_language}</div>
+      <div className="movie__date movie__info">{props.release_date}</div>
+      <div className="movie__id movie__info">영화ID : {props.id}</div>
     </div>
   );
 }
