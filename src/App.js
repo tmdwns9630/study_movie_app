7aead499c1887e37d4aa3509621e1a03e231da5d
CSJ <tmdwns9630@gmail.com>
2022-09-19 17:09:47 +0900

    왜 되는건데 시바 심지어 movies 콘솔로그는 그대로 비어있는데 왜 html에선 뜨는거야

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 //import ClickCounter from "./components/ClickCounter";
 //로딩 화면의 원리를 간단하게 알아보자.
 function App(props) {
-  //const [loading, setloading] = useState(true);
+  const [loading, setloading] = useState(true);
   const [movies, setMovies] = useState([]);
 
   async function getMovieAPI() {
@@ -17,14 +17,9 @@ function App(props) {
     console.log(result.data.results);
     console.log(result.data.results[0].title);
     console.log(`무비스 before : ${movies}`);
-
-    setMovies(function (mv) {
-      return result.data.results;
-    });
+    setMovies(result.data.results);
     console.log(`무비스 after : ${movies}`);
     console.log(`무비스0 타이틀 : ${movies[0].title}`);
-    //>Promise
-    //ㄴ 호출했을 때 시간이 걸리는 경우.
   }
 
   //뭔가를 기다려야할 경우
@@ -32,36 +27,41 @@ function App(props) {
   //await : 기다려야할 대상이 있는 라인을 명시.
   //result.data에 postman에서 봤던 데이터가 객체화되어서 들어와있다.
   getMovieAPI();
+
   return (
-    <Fragment>
-      <p>
-        {/* https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg */}
-        <div>hello world</div>
-        <div>제목 : {movies[0].title} </div>
-        <div>줄거리 : {movies[0].overview} </div>
-        <div>평점 : {movies[0].vote_average}</div>
-        <div>성인영화 : {movies[0].vote_average}</div>
-        <div>언어 : {movies[0].original_language}</div>
-        <div>개봉일 : {movies[0].release_date}</div>
-        <img
-          src={`https://image.tmdb.org/t/p/original/${movies[0].poster_path}`}
-          alt="loading"
-        ></img>
-      </p>
-      <p>
-        <div>hello world</div>
-        <div>제목 : {movies[1].title} </div>
-        <div>줄거리 : {movies[1].overview} </div>
-        <div>평점 : {movies[1].vote_average}</div>
-        <div>성인영화 : {movies[1].vote_average}</div>
-        <div>언어 : {movies[1].original_language}</div>
-        <div>개봉일 : {movies[1].release_date}</div>
-        <img
-          src={`https://image.tmdb.org/t/p/original/${movies[0].poster_path}`}
-          alt="loading"
-        ></img>
-      </p>
-    </Fragment>
+    <>
+      {0 < movies.length ? (
+        <Fragment>
+          <p>
+            {/* https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg */}
+            <div>hello world</div>
+            <div>제목 : {movies[0].title} </div>
+            <div>줄거리 : {movies[0].overview} </div>
+            <div>평점 : {movies[0].vote_average}</div>
+            <div>성인영화 : {movies[0].vote_average}</div>
+            <div>언어 : {movies[0].original_language}</div>
+            <div>개봉일 : {movies[0].release_date}</div>
+            <img
+              src={`https://image.tmdb.org/t/p/original/${movies[0].poster_path}`}
+              alt="loading"
+            ></img>
+          </p>
+          <p>
+            <div>hello world</div>
+            <div>제목 : {movies[1].title} </div>
+            <div>줄거리 : {movies[1].overview} </div>
+            <div>평점 : {movies[1].vote_average}</div>
+            <div>성인영화 : {movies[1].vote_average}</div>
+            <div>언어 : {movies[1].original_language}</div>
+            <div>개봉일 : {movies[1].release_date}</div>
+            <img
+              src={`https://image.tmdb.org/t/p/original/${movies[0].poster_path}`}
+              alt="loading"
+            ></img>
+          </p>
+        </Fragment>
+      ) : null}
+    </>
   );
 }
 export default App;
