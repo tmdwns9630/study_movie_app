49eaa3af3338e4ff3ae7f1e7c7ad42ee907dffd4
CSJ <tmdwns9630@gmail.com>
2022-09-21 14:05:06 +0900

    유튜브 카피 만들기 - 동영상 창까지 만들었음 ㄴ 썸네일 영상시간은 위치 배정이 안됐음
    다음 만들 것
    - 하단 메뉴(홈, shorts, + , 구독, 보관함)
    - shorts 목록

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -2,6 +2,7 @@ import "./Youtube.css";
 
 function Youtube(props) {
   return (
+    // 최상단----------------------------------
     <div className="yt__main">
       <div className="yt__top">
         <div>
@@ -40,16 +41,173 @@ function Youtube(props) {
           </span>
         </div>
       </div>
+      {/* ---------top2: 유튜브 로고 ---------------- */}
       <div className="yt__top2">
         <div className="yt__top2_left">
-          <span>탐색</span>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_play_button_icon_%282013%E2%80%932017%29.svg/2560px-YouTube_play_button_icon_%282013%E2%80%932017%29.svg.png"
+            width="35px"
+            height="30px"
+          ></img>
+          Premium
         </div>
         <div className="yt__top2_right">
-          <span>전체</span>
-          <span>게임</span>
-          <span>믹스</span>
-          <span>음악</span>
-          <span>아니메</span>
+          <span className="yt__top2_right_type">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+              />
+            </svg>
+          </span>
+          <span className="yt__top2_right_type">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+              />
+            </svg>
+          </span>
+          <span className="yt__top2_right_type">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+          </span>
+          <span className="yt__top2_right_type yt__top2_right_type_User">
+            승준
+          </span>
+        </div>
+        {/* top3 : 탐색 ---------------------------- */}
+      </div>
+      <div className="yt__top3">
+        <div className="yt__top3_left">
+          <span>⭐탐색</span>
+        </div>
+        <div className="yt__top3_Line"></div>
+        <div className="yt__top3_right ">
+          <span className="yt__top3_right_type">전체</span>
+          <span className="yt__top3_right_type">게임</span>
+          {/* <span className="yt__top3_right_type">새로운 맞춤 동영상</span> */}
+          <span className="yt__top3_right_type">믹스</span>
+          <span className="yt__top3_right_type">음악</span>
+          <span className="yt__top3_right_type">Animation</span>
+        </div>
+      </div>
+      {/* 4 : 동영상1----------------------- */}
+      <div className="yt__video">
+        <div className="yt__video_Thumbnail">
+          <img src="https://i.ytimg.com/vi/2Gb9RpZrFDY/maxresdefault.jpg"></img>
+          <div className="yt__video_Thumbnail_runTime">10:21</div>
+        </div>
+        <div className="yt__video_bar">
+          <div className="yt_video_info_uploaderProfile">
+            <img src="https://yt3.ggpht.com/ytc/AMLnZu9vwV4Dgc3o-qdpVCPP2iy0ok-APj4DJyyPMWQtvw=s900-c-k-c0x00ffffff-no-rj"></img>
+          </div>
+          <div className="yt_video_info_text">
+            <div className="yt_video_info_title">
+              <div className="yt_video_info_title_text">
+                커비야... 너 왜 눈을 그렇게 떠...? [커비 스페샬]
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+                />
+              </svg>
+            </div>
+
+            <div className="yt_video_info_uploaderInfo">
+              <span className="yt_video_info_uploaderInfo_uploaderName">
+                김도랜드
+              </span>
+              <span>·</span>
+              <span className="yt_video_info_uploaderInfo_count">
+                조회수 11만회
+              </span>
+              <span>·</span>
+              <span className="yt_video_info_uploaderInfo_time">2년 전</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* 4 : 동영상2----------------------- */}
+      <div className="yt__video">
+        <div className="yt__video_Thumbnail">
+          <img src="https://i.ytimg.com/vi/dRN7HLN1lfQ/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAlAQupieZouLgeNqtYln3tVojcBQ"></img>
+          <div className="yt__video_Thumbnail_runTime">10:21</div>
+        </div>
+        <div className="yt__video_bar">
+          <div className="yt_video_info_uploaderProfile">
+            <img src="https://yt3.ggpht.com/ytc/AMLnZu9T1gKkJqvCFWdDPAPiwZe2urmNwg85Zma-9HMZDw=s900-c-k-c0x00ffffff-no-rj"></img>
+          </div>
+          <div className="yt_video_info_text">
+            <div className="yt_video_info_title">
+              <div className="yt_video_info_title_text">
+                AVGN도 분노한 전설의 똥게임 [이름없는 마을
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+                />
+              </svg>
+            </div>
+
+            <div className="yt_video_info_uploaderInfo">
+              <span className="yt_video_info_uploaderInfo_uploaderName">
+                긴도랜드
+              </span>
+              <span>·</span>
+              <span className="yt_video_info_uploaderInfo_count">
+                조회수 18만회
+              </span>
+              <span>·</span>
+              <span className="yt_video_info_uploaderInfo_time">2년 전</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
